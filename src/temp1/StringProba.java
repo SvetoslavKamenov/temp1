0a285d47b51b359363be0ba13054235fe25e9946
svet-kam <sv_kamenov@yahoo.com>
2017-01-13 19:54:53 +0200

    no message

---
+++ src/temp1/StringProba.java
@@ -0,0 +1,13 @@
+package temp1;
+
+public class StringProba {
+
+	public static void main(String[] args) {
+		 String a = "a";
+		 a += "b";
+		 System.out.println(a);
+				 
+
+	}
+
+}
---
+++ src/temp1/Triangle.java
@@ -0,0 +1,13 @@
+package temp1;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		double a = 5;
+		double b =0;
+		System.out.println(a/b);
+		
+
+	}
+
+}
